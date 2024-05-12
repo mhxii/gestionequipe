bbdbb7ff4f267e842ac6648c4758994272db3710
mhxii <mhxii105@gmail.com>
2024-05-12 22:38:01 +0000

    INIT

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+const style = "tex";
+function HeaderForLogin() {
+    return(
+        <div class="headerForLogin">
+            <img>LOGO EQUIPE</img>
+        </div>
+    );
+}
+
+export default HeaderForLogin;
